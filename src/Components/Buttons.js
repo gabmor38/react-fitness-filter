fb9c22ee18b7afb76e5f8a9fd8beb4aebcf45542
gabmor38 <Gabriela.morenor@gmail.co>
2022-03-02 16:49:58 -0500

    created new ALL category button

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,24 +1,31 @@
 import Data from ".//Data"
 
 export default function Buttons(props) {
-  const {setItem, exerciseItems} = props;
+  const {setItem, exerciseCategory} = props;
 
   console.log("this setItem", setItem)
 
   return(
     <>
       <div className="d-flex justify-content-center">
-        {exerciseItems.map((exercise, id) => {
+        {exerciseCategory.map((exercise, id) => {
           return (
+            //this one makes a button based on the filtered category - bodyPart
             <button
             className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
             key={id}
             >
             {exercise}
             </button>
-          )
+          );
         }
         )}
+        //this button will select all of the categories
+        <button className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
+          onClick={() => setItem(Data)}
+        >
+        All Categories
+        </button>
       </div>
     </>
   )
